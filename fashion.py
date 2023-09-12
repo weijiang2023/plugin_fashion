f37d58060f3f5edb2a84cc93a9dfdd9c03785c82
江纬 <wei@algmon.com>
2023-09-12 19:05:18 +0800

    Update fashion

--- fashion.py
+++ fashion.py
@@ -12,7 +12,7 @@ from plugins import *
     name="Fashion",
     desire_priority=-1,
     hidden=True,
-    desc="A simple plugin developped by algmon saying fashion",
+    desc="A simple plugin saying fashion",
     version="0.1",
     author="weijiang2009",
 )
@@ -66,10 +66,11 @@ class Fashion(Plugin):
             e_context["reply"] = reply
             e_context.action = EventAction.BREAK  # 事件结束，进入默认处理逻辑，一般会覆写reply
 
-        if content == "End":
+        if content == "Fashion":
             # 如果是文本消息"End"，将请求转换成"IMAGE_CREATE"，并将content设置为"The World"
             e_context["context"].type = ContextType.IMAGE_CREATE
-            content = "The World"
+            # TODO: 更精准自然语言编程
+            content = "A lady or man walking down the runway with latest fashion"
             e_context.action = EventAction.CONTINUE  # 事件继续，交付给下个插件或默认逻辑
 
     def get_help_text(self, **kwargs):
